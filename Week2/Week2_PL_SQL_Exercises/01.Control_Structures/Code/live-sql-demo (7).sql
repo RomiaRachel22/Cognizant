83be90064e36221f0e969977ee4657c8fe283cd7
RomiaRachel22 <115524827+RomiaRachel22@users.noreply.github.com>
2025-06-27 02:05:35 +0530

    Delete Week2 directory

--- Week2/Week2_PL_SQL_Exercises/01.Control_Structures/Code/live-sql-demo (7).sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Customers (
-    CustomerID      NUMBER PRIMARY KEY,
-    Name            VARCHAR2(100),
-    Age             NUMBER,
-    Balance         NUMBER,
-    LoanInterestRate NUMBER(5,2),
-    IsVIP           VARCHAR2(5)
-);
