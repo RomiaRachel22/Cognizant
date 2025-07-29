6496f957ffc84b5214a2afd57405634b38047bbc
RomiaRachel22 <115524827+RomiaRachel22@users.noreply.github.com>
2025-07-29 21:19:04 +0530

    Add files via upload

---
+++ Week6/Week6_React/4. ReactJS-HOL/Code/blogapp/src/Posts.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Post from './Post';
+
+class Posts extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        };
+    }
+
+    loadPosts() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(data => {
+                const postList = data.map(p => new Post(p.id, p.title, p.body));
+                this.setState({ posts: postList });
+            })
+            .catch(error => {
+                alert("Error while loading posts: " + error.message);
+            });
+    }
+
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        alert("An error occurred: " + error.toString());
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Posts</h1>
+                {this.state.posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Posts;
